62e3e9a67cb6664b577eb1497a8f5fe861fe8b2f
ykx0xky <nsymsk@gmail.com>
2025-10-07 12:18:14 +0900

    Add files via upload

---
+++ lp_wrstick/js/slider.js
@@ -0,0 +1,11 @@
+$('.slider').slick({
+    autoplay: true,       //自動再生
+    autoplaySpeed: 2000,  //自動再生のスピード
+    speed: 800,           //スライドするスピード
+    dots: false,           //スライド下のドット
+    arrows: true,         //左右の矢印
+    infinite: true,       //永久にループさせる
+	//center modeとcenterPadding を指定
+    centerMode: true,
+    centerPadding: "20%",
+});
